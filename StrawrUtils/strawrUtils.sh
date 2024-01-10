8107e4f8ed6ecc0ea13aa285e0b3cf17abd7e05e
Jordan Roberts <jordanroberts@Jordans-MacBook-Air.local>
2024-01-10 05:22:42 +0000

    added open vscode function

--- StrawrUtils/strawrUtils.sh
+++ StrawrUtils/strawrUtils.sh
@@ -9,6 +9,20 @@ title(){
 EOF
 }
 
+openVscode(){
+    if command -v code &> /dev/null; then
+        if [ -d "$1" ]; then
+            code "$1"
+        else
+            echo "SRC ERROR: path to vscode doesn't exist"
+            exit
+        fi
+    else
+        echo "Vscode needs to be added to path"
+        exit
+    fi
+}
+
 mainMenu(){
     OPTIONS=("Open" "Create" "Playground" "temporary")
     PS3="Select an option (1-${#OPTIONS[@]}): "
@@ -17,23 +31,24 @@ mainMenu(){
             1)
                 echo "You selected Option 1"
                 break
-                ;;
+            ;;
             2)
                 echo "You selected Option 2"
                 break
-                ;;
+            ;;
             3)
-                echo "You selected Option 3"
+                PLAYGROUND_PATH="/Users/jordanroberts/Documents/dev/Playground"
+                openVscode $PLAYGROUND_PATH
                 break
-                ;;
+            ;;
             4)
                 echo "Exiting the script."
                 exit
-                ;;
+            ;;
             *)
                 echo "Invalid choice. Please try again."
                 break
-                ;;
+            ;;
         esac
     done
 }
