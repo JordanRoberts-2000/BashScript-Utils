b1e296103e8d8c8c8783686e9286efcad618a534
Jordan Roberts <jordanroberts@Jordans-MacBook-Air.local>
2024-01-14 02:53:48 +0000

    feat: delete all node_modules within temp dir

--- StrawrUtils/features/temporary.sh
+++ StrawrUtils/features/temporary.sh
@@ -10,7 +10,19 @@ temporary(){
                     createProject $TEMP_FOLDER_PATH
                 ;;
                 2)
-                    break 2
+                    while true; do
+                        title
+                        echo -n "are you sure? (y/n)"
+                        read ARE_YOU_SURE
+                        if [ "$ARE_YOU_SURE" == "y" ]; then
+                            find $TEMP_FOLDER_PATH -name "node_modules" -type d -exec rm -rf {} +
+                            break 3
+                        fi
+                        if [ "$ARE_YOU_SURE" == "n" ]; then
+                            break 2
+                        fi
+                        
+                    done
                 ;;
                 3)
                     selectDirsFromPath $TEMP_FOLDER_PATH
